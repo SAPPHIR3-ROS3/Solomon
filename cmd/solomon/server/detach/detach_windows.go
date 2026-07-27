ddad6016f6e3af048891a46560d9e6bae3fcb7f5
SAPPHIR3-ROS3 <patrizioonida@gmail.com>
2026-07-27 04:22:21 +0200

    Fix Windows server detach flag

--- cmd/solomon/server/detach/detach_windows.go
+++ cmd/solomon/server/detach/detach_windows.go
@@ -7,6 +7,8 @@ import (
 	"syscall"
 )
 
+const detachedProcess = 0x00000008
+
 func Configure(cmd *exec.Cmd) {
-	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP | syscall.DETACHED_PROCESS}
+	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP | detachedProcess}
 }
